770b9fed4aa3d81976f5a5efa98435f50f51dedd
Falakhe <57628849+Falakhe203@users.noreply.github.com>
2023-12-30 02:45:37 +0200

    main.tf and variables.tf commit

---
+++ data/modules/common/aws-glue-table-csv/main.tf
@@ -0,0 +1,59 @@
+resource "aws_glue_catalog_table" "table" {
+    name = var.table_name 
+    database_name = var.database_name
+    catalog_id = var.glue_catalog_aws_account_id
+    table_type = "EXTERNAL TABLE"
+    parameters = var.table_parameters
+    owner = "owner"
+
+    storage_descriptor {
+      location = var.s3_storage_location
+      input_format = "org.pache.hadoop.mapred.TextInputFormat"
+      output_format = "org.apach.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+      parameters = var.table_parameters
+
+    #   serialization with qoute support
+        ser_de_info {
+            serialization_library = var.serialization_library
+
+            parameters = var.serialization_parameters
+        }
+
+        dynamic "columns" {
+            for_each = var.columns
+            content {
+                name = columns.value[0]
+                type = columns.value[1]
+            }
+        }
+    }
+
+    dynamic "partition_keys" {
+        for_each = var.partition_keys
+        content {
+            name = partition_keys.value[0]
+            type = partition_keys.value[1]
+        }
+    }
+
+    lifecycle {
+        #   these values are set by Glue Crawler and shouldn't be reset by terraform
+        ignore_changes = [
+            parameters["CrawlerSchemaDeserializerVersion"],
+            parameters["CrawlerSchemaSerializerVersion"],
+            parameters["averageRecordSize"],
+            parameters["objectCount"],
+            parameters["recordCount"],
+            parameters["sizeKey"],
+            parameters["UPDATED_BY_CRAWLER"],
+            storage_descriptor.0.number_of_buckets,
+            storage_descriptor.0.parameters["CrawlerSchemaDeserializerVersion"],
+            storage_descriptor.0.parameters["CrawlerSchemaSerializerVersion"],
+            storage_descriptor.0.parameters["averageRecordSize"],
+            storage_descriptor.0.parameters["objectCount"],
+            storage_descriptor.0.parameters["recordCount"],
+            storage_descriptor.0.parameters["sizeKey"],
+            storage_descriptor.0.parameters["UPDATED_BY_CRAWLER"],
+        ]
+    }
+}
---
+++ data/modules/common/aws-glue-table-csv/variables.tf
@@ -0,0 +1,61 @@
+variable "table_name" {
+    description = "name of Glue Data Catalog table to be created"
+    type = string
+}
+
+variable "database_name" {
+    description = "name of Glue Data Catalog database for table"
+    type = string
+}
+
+variable "glue_catalog_aws_account_id" {
+    description = "AWS account id of the Glue Data Catalog"
+    type = string
+}
+
+variable "s3_storage_location" {
+    description = "s3 path to source data"
+    type = string
+}
+
+variable "columns" {
+    description = "list of table columns in [name, type] tuples"
+    type = list(list(string))
+    default = []
+}
+
+variable "partition_keys" {
+    description = "list of table pertition in [name, type] tuples"
+    type = list(list(string))
+    default = []
+}
+
+variable "table_parameters" {
+    description = "Parameters for Glue Data Catalog table"
+    type = map(any)
+    default = {
+        areColumnsQuoted = "false"
+        classification = "csv"
+        columnsOrdered = "true"
+        compressionType = "none"
+        delimeter = ","
+        typeofData = "file"
+        "skip.header.line.count" = 1
+    }
+}
+
+variable "serialization_library" {
+    description = "Glue Data Catalog serialization library"
+    type = string
+    default = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+}
+
+variable serialization_parameters {
+    description = "Parameters for serialization library"
+    type = map(string)
+    default = {
+        escapeChar = "\\"
+        quoteChar = "\""
+        seperatorChar = ","
+    }
+}
